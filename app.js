b0cc871ec09dc41f2843d56c9f01df0f1e1074df
Izzy <izzyreiff@gmail.com>
2023-09-19 17:36:48 -0400

    Aesthetics

--- app.js
+++ app.js
@@ -51,12 +51,41 @@ function isFormValid() {
 function submitForm() {
     if (isFormValid()) {
         // Form is valid, you can submit it here or perform other actions
-        alert("Form is valid and can be submitted!");
+        // alert("Form is valid and can be submitted!");
         // Clear the error message
         errorMessage.textContent = "";
     } else {
         errorMessage.textContent = "At least two usernames must be submitted!";
     }
+    numUsers = 0;
+    const userInput1 = userInputs[0];
+    const value1 = userInput1.value;
+    if (value1 !== "") {
+        console.log("Value of User1 input:", value1);
+        numUsers++;
+    }
+
+    const userInput2 = userInputs[1];
+    const value2 = userInput2.value;
+    if (value2 !== "") {
+        console.log("Value of User2 input:", value2);
+        numUsers++;
+    }
+
+    const userInput3 = userInputs[2];
+    const value3 = userInput3.value;
+    if (value3 !== "") {
+        console.log("Value of User3 input:", value3);
+        numUsers++;
+    }
+
+    const userInput4 = userInputs[3];
+    const value4 = userInput4.value;
+    if (value4 !== "") {
+        console.log("Value of User4 input:", value4);
+        numUsers++;
+    }
+    console.log(numUsers)
 }
 
 // Enable form submission on button click
