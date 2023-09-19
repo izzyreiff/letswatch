0febb14a4c262d459635284b5b5dda194004c685
Izzy <izzyreiff@gmail.com>
2023-09-19 15:17:46 -0400

    Add index, app, style

---
+++ app.js
@@ -0,0 +1,47 @@
+// JavaScript to toggle between light and dark modes
+const body = document.body;
+const modeToggle = document.getElementById("modeToggle");
+
+modeToggle.addEventListener("click", () => {
+    body.classList.toggle("dark-mode");
+});
+
+// Form validation and submission
+const submitButton = document.getElementById("submitButton");
+const userInputs = [document.getElementById("User1"), document.getElementById("User2"), document.getElementById("User3"), document.getElementById("User4")];
+const errorMessage = document.getElementById("errorMessage");
+
+// Function to check if at least two user inputs are filled
+function isFormValid() {
+    let filledCount = 0;
+    for (const input of userInputs) {
+        if (input.value.trim() !== "") {
+            filledCount++;
+        }
+    }
+    return filledCount >= 2;
+}
+
+// Function to handle form submission
+function submitForm() {
+    if (isFormValid()) {
+        // Form is valid, you can submit it here or perform other actions
+        alert("Form is valid and can be submitted!");
+        // Clear the error message
+        errorMessage.textContent = "";
+    } else {
+        errorMessage.textContent = "At least two usernames must be given!";
+    }
+}
+
+// Enable form submission on button click
+submitButton.addEventListener("click", submitForm);
+
+// Enable form submission on Enter key press
+userInputs.forEach((input) => {
+    input.addEventListener("keyup", (event) => {
+        if (event.key === "Enter") {
+            submitForm();
+        }
+    });
+});
---
+++ overlap.py
@@ -0,0 +1,82 @@
+import math
+from selenium import webdriver
+from collections import defaultdict
+from bs4 import BeautifulSoup as bs
+
+
+def pull_url(url):
+
+    STANDARD_PAGE_SIZE = 28
+
+    browser = webdriver.Chrome()
+    target_url = url
+    browser.get(target_url)
+
+    html = browser.execute_script("return document.body.innerHTML")
+    page = bs(html, 'html.parser')
+
+    num_films = page.find('h1', attrs={'class': 'section-heading'}).get_text()[-8:]
+    total_count = int(num_films[0:2])
+
+    page_count = math.ceil(total_count / STANDARD_PAGE_SIZE)
+
+    movie_list = []
+
+    for movie in page.find_all('span', attrs={'class': 'frame-title'}):
+            movie_list.append(movie.get_text())
+
+    if page_count > 1:
+
+        for i in range(2, page_count+1):
+            browser.get(url + '/page/' + str(i))
+            html = browser.execute_script("return document.body.innerHTML")
+            page = bs(html, 'html.parser')
+            for movie in page.find_all('span', attrs={'class': 'frame-title'}):
+                movie_list.append(movie.get_text())
+
+        return movie_list
+    else:
+        return movie_list    
+
+
+def process_friends(friend_list):
+
+    movie_bag = []
+
+    for name in friend_list:
+        search_path = 'https://letterboxd.com/' + name + '/watchlist/'
+        movie_bag.append(pull_url(search_path))    
+
+    return movie_bag
+
+
+def find_films_in_common(film_bag):
+
+    film_dict = defaultdict(int)
+
+    for wishlist in film_bag:
+        for film in wishlist:
+            film_dict[film] += 1
+
+    desc_dict = sorted(film_dict.items() , key=lambda t : t[1] , reverse=True)
+    
+    output_file = open("movies-in.txt", "w")
+
+    for k,v in desc_dict:
+        output_file.write(k + ' ' + str(v) + '\n')
+
+    output_file.close
+
+    return
+
+
+def main():
+
+    FRIENDS = ['ireiff', 'emilymki', 'smreiff']
+
+    bag = process_friends(FRIENDS)
+    find_films_in_common(bag)
+
+
+if __name__ == "__main__":
+    main()
